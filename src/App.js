c82e9a0b333b784a521fd2be6d95cb7ef3ec12eb
AzShayak <shayakanis@gmail.com>
2021-09-25 22:12:06 +0600

    nothing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import "./App.css";
-import Components from "./Components/Components";
-import { BrowserRouter,Redirect,Route,Switch } from "react-router-dom";
-
+import './App.css';
+import Components from './Components/Components';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Components />
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Components />
+		</BrowserRouter>
+	);
 }
 
 export default App;
